557f9df787585e6c5ee77175910d347071b7e91f
Dennif <dennif.pa@gmail.com>
2025-06-25 14:59:30 -0700

    card mapping corrected and conection routes implemented

--- src/services/boardApi.js
+++ src/services/boardApi.js
@@ -1,11 +1,12 @@
 
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000"; 
+const BASE_URL = "https://inspo-board-api-cyws.onrender.com";
 
 export const getBoards = () => {
     return axios.get(`${BASE_URL}/boards`);
 };
 
-export const createBoard = (boardData) =>
-    axios.post(`${BASE_URL}/boards`, boardData);
+export const createBoard = (boardData) => {
+    return axios.post(`${BASE_URL}/boards`, boardData);
+};
